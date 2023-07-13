80e0910a017a1788a72f43e714755608e1dde88d
Seoa Kim <seoa521@gmail.com>
2023-07-13 18:31:16 +0900

    [feat] 글 작성 페이지 UI 구현

--- src/pages/PostCreate.jsx
+++ src/pages/PostCreate.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function PostCreate() {
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.prevent.default();
+
+    console.log("Title: ", title);
+    console.log("Content: ", content);
+
+    setTitle("");
+    setContent("");
+  };
+
   return (
-    <div>
-      <h1>여기는 작성 페이지</h1>
+    <div className="blog-editor">
+      <h1>Create a New Blog Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={handleContentChange}
+          />
+        </div>
+        <button type="submit">Post</button>
+      </form>
     </div>
   );
-}
+};
