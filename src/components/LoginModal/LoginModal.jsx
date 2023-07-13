7c2cf2c2d9866af5164a96d09bbdea1482c7ce0a
confidential-nt <youyoy689@gmail.com>
2023-07-14 00:06:06 +0900

    [fix] 로그인 모달의 password input autoComplete 설정

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -4,7 +4,7 @@ export default function LoginModal() {
   return (
     <form>
       <input type="email" placeholder="이메일" />
-      <input type="password" placeholder="비밀번호" />
+      <input type="password" placeholder="비밀번호" autoComplete="on" />
     </form>
   );
 }
