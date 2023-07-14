93ac982621071d392351de049e7e861a441f64d2
confidential-nt <youyoy689@gmail.com>
2023-07-14 15:56:12 +0900

    [feat] 헤더에 각 페이지 바로가기 추가 (상세페이지 바로가기는 포스트 카드가 구현 완료되는 즉시 삭제 예정)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header({ onClick }) {
   const handleClick = () => {
@@ -9,6 +10,13 @@ export default function Header({ onClick }) {
       <button type="button" onClick={handleClick}>
         로그인
       </button>
+      <Link to="/" style={{ "margin-right": "10px" }}>
+        메인페이지
+      </Link>
+      <Link to="/posts/123213" style={{ "margin-right": "10px" }}>
+        상세페이지
+      </Link>
+      <Link to="/posts/create">작성페이지</Link>
     </header>
   );
 }
