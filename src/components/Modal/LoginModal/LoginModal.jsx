ff3542a1694c8d21c78a135b44144712aa136715
confidential-nt <youyoy689@gmail.com>
2023-07-19 09:29:30 +0900

    [feat] 로그인 기본 기능 구현

--- src/components/Modal/LoginModal/LoginModal.jsx
+++ src/components/Modal/LoginModal/LoginModal.jsx
@@ -1,58 +1,61 @@
 import React, { useState } from "react";
 import Modal from "../Modal";
 import { BtnId } from "../../../constant/btn-id";
-import SignUpModal from "../SignUpModal/SignUpModal";
+import axios from "axios";
+import { useUserContext } from "../../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
-export default function LoginModal() {
-  const [display, setDisplay] = useState(BtnId.LOG_IN);
+export default function LoginModal({ onClick }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {
-    setDisplay(display === BtnId.SIGN_UP ? BtnId.LOG_IN : BtnId.SIGN_UP);
+  const { logUserIn } = useUserContext();
+
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    onClick(BtnId.SIGN_UP);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // 로그인 관련
+    const result = await axios.post("http://3.38.117.203/login", {
+      email,
+      password,
+    });
+
+    logUserIn(result.data.message);
+    onClick(null);
+    navigate("/");
   };
 
   return (
-    <div>
-      {display === BtnId.LOG_IN ? (
-        <Modal>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              placeholder="이메일"
-              autoComplete="on"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              placeholder="비밀번호"
-              autoComplete="on"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <button type="submit">로그인</button>
-          </form>
-          <p>
-            아직 회원이 아니십니까?
-            <button type="button" onClick={handleClick}>
-              회원가입
-            </button>
-          </p>
-        </Modal>
-      ) : (
-        <>
-          <SignUpModal />
-          <button onClick={handleClick}>뒤로가기</button>
-        </>
-      )}
-    </div>
+    <Modal onClick={onClick}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="이메일"
+          autoComplete="on"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          autoComplete="on"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">로그인</button>
+      </form>
+      <p>
+        아직 회원이 아니십니까?
+        <button type="button" onClick={handleClick}>
+          회원가입
+        </button>
+      </p>
+    </Modal>
   );
 }
