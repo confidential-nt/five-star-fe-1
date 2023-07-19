fcf909f19c951df8bf32b36bf56055af20afcbe4
confidential-nt <youyoy689@gmail.com>
2023-07-19 09:28:11 +0900

    [refactoring] MainHeader 제거

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import MainHeader from "../components/MainHeader";
 
 export default function Main() {
-  return (
-    <div>
-      <MainHeader />
-    </div>
-  );
+  return <div></div>;
 }
