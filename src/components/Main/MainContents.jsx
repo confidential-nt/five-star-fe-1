402853084b68d1edbfd2cbf42582394876a2d2a7
did-jiwon <puding0712@naver.com>
2023-07-21 16:23:35 +0900

    게시물클릭시해당게시물로이동구현

--- src/components/Main/MainContents.jsx
+++ src/components/Main/MainContents.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const MainContents = ({ sortBy }) => {
     const [data, setData] = useState({});
     const [page, setPage] = useState(1); // 현재 페이지
-    const [pageLimit, setPageLimit] = useState(5); // 페이지 당 게시물 수
+    const [pageLimit, setPageLimit] = useState(10); // 페이지 당 게시물 수
     const [total, setTotal] = useState(0);
+    const navigate = useNavigate();
     
     const numPages = Math.ceil(total / pageLimit);
     const offset = (page-1) * pageLimit;
@@ -25,6 +27,7 @@ const MainContents = ({ sortBy }) => {
 
                 setData(sortedData);
                 setTotal(sortedData.length);
+                setPageLimit(response.data.pageable.size);
             } catch(error) {
                 console.error('Error fetching local data:', error);
             }
@@ -33,18 +36,24 @@ const MainContents = ({ sortBy }) => {
         fetchData();
     }, [sortBy]);
 
+    const handleClick = (itemId) => {
+        navigate(`/posts/${itemId}`);
+    }
+
     return (
         <div className="container">
             <div className="contents">
                 {data.length > 0 && (
                     <ul>
                         {data.slice(offset, offset + pageLimit).map((item) => (
-                            <li key={item.id}>
-                                <h1>{item.title}</h1>
-                                <p>{item.content}</p>
-                                <p>{item.createAt}</p>
-                                <p>{item.modifiedAt}</p>
-                            </li>
+                            <>
+                                <li key={item.id} >
+                                    <h1 onClick={() => handleClick(item.id)} style={{cursor: 'pointer'}}>{item.title}</h1>
+                                    <p>{item.content}</p>
+                                    <p>{item.createAt}</p>
+                                    <p>{item.modifiedAt}</p>
+                                </li>
+                            </>
                         ))}
                     </ul>
                 )
