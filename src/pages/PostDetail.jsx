8fb1bb8991dd6ba57f1a788242877b9593797535
confidential-nt <youyoy689@gmail.com>
2023-07-24 11:13:40 +0900

    [feat] 실제 상세페이지로 들어가도록 수정

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from "react";
 import timeAgo from "../utils/time-ago";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function PostDetail() {
   const [post, setPost] = useState();
   const { postid } = useParams();
 
   useEffect(() => {
-    fetch("/data/posts-detail.json") // 나중에는 axios를 통한 실제 db와의 get요청으로 바꿀 것. get /posts/postsid
-      .then((res) => res.json())
-      .then((post) => setPost(post[0]));
+    axios
+      .get(`http://3.38.117.203/posts/${postid}`)
+      .then((post) => setPost(post));
   }, [postid]);
 
   const navigate = useNavigate();
 
   const handleEdit = () => {
-    navigate("/posts/edit", {
-      // 아니면 edit이라는 새로운 path 추가. -> 나누는 게 나을 것 같다.
+    navigate(`/posts/${postid}/edit`, {
       state: {
-        post,
+        post: post.data,
       },
     });
   };
 
-  const handleDelete = () => {
-    // 나중에는 aixos를 통한 실제 db와의 delete요청으로 바꿀 것.
-    // delete /posts/{postid}
+  const handleDelete = async () => {
+    await axios.delete(`http://3.38.117.203/posts/${postid}`);
   };
 
   return (
@@ -37,8 +36,8 @@ export default function PostDetail() {
           <span>
             수정된 날짜: {timeAgo(post.modifiedAt, navigator.language)}
           </span>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
+          <h1>{post.data.title}</h1>
+          <p>{post.data.content}</p>
           <div>
             <button onClick={handleEdit}>수정하기</button>
             <button onClick={handleDelete}>삭제하기</button>
