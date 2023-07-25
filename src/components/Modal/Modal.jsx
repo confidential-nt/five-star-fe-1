713cbb1580e768133108e7584c8dd1a06b5afd1b
confidential-nt <youyoy689@gmail.com>
2023-07-25 14:02:51 +0900

    [feat] 모달 뜰 때 스크롤 안되도록 구현

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Modal.module.css";
+
+const onScroll = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+};
 
 export default function Modal({ onClick, children }) {
   const handleClick = () => {
     onClick(null);
   };
+
+  useEffect(() => {
+    window.addEventListener("wheel", onScroll, {
+      passive: false,
+    });
+
+    return () => window.removeEventListener("wheel", onScroll);
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
